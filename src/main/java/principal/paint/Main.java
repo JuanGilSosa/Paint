4fd8727ff51d4a7e84be602c1bfbbaa833e8f83a
Juan Gil Sosa <juanmanuels499@gmail.com>
2021-10-11 18:13:20 -0300

    creacion de mainframe

--- src/main/java/principal/paint/Main.java
+++ src/main/java/principal/paint/Main.java
@@ -4,8 +4,10 @@ package principal.paint;
  * lunes 11, octubre 2021
  * @author juanm
  */
+
+import principal.paint.Frames.MainFrame;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        MainFrame myFrame = new MainFrame();
     }
 }
