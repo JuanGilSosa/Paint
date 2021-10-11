849401eb3e8258d0bb54936bfa8293c3fdc2d218
Juan Gil Sosa <juanmanuels499@gmail.com>
2021-10-11 17:54:48 -0300

    Clase Main

---
+++ src/main/java/principal/paint/Main.java
@@ -0,0 +1,11 @@
+package principal.paint;
+
+/**
+ * lunes 11, octubre 2021
+ * @author juanm
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
